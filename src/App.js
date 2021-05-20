b9f74912d9d2e4ef3de881d9c3cbdedb0f9d1d86
elihimself <tamosauskas09@gmail.com>
2021-05-20 20:14:24 +0300

    #1 - Fetching Pokemon Data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import PokemonList from "./components/PokemonList/PokemonList";
 
+// hello world
 export default function App() {
   return <PokemonList />;
 }
--- src/components/PokemonList/PokemonList.js
+++ src/components/PokemonList/PokemonList.js
@@ -1,7 +1,43 @@
-import * as React from "react";
+import React, { useEffect, useState, useCallback } from "react";
+
+const BASE_URL = "https://pokeapi.co/api/v2/pokemon";
+const FETCH_URL = `${BASE_URL}?limit=15`;
 
 const PokemonList = () => {
-  return <div>Pokemon list goes here</div>;
+  const [loading, setLoading] = useState(true);
+  const [pokemonData, setPokemonData] = useState([]);
+  const [err, setErr] = useState(null);
+
+  const fetchData = useCallback(async () => {
+    try {
+      const res = await fetch(FETCH_URL);
+      const parsedData = await res.json();
+      const { results } = parsedData;
+      setPokemonData(results);
+      setLoading(false);
+    } catch (error) {
+      setErr("Trouble while fetching Pokemon data. Try again later.");
+      setLoading(false);
+    }
+  }, []);
+
+  useEffect(() => {
+    fetchData();
+  }, [fetchData]);
+
+  if (err) {
+    return <div>{err}</div>;
+  }
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (pokemonData.length === 0) {
+    return <div>There are no Pokemon to show yet...</div>;
+  }
+
+  return pokemonData.map((pokemon, i) => <div key={i}>{pokemon.name}</div>);
 };
 
 export default PokemonList;
