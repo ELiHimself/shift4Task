d2fd1fff26d34ef9834390cb8c45402ee1fda055
Eli Tamosauskas <eli.tamosauskas@redacreltd.com>
2021-05-21 11:42:50 +0300

    #4 - Pokemon Details Page - Feat - Add routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import React from "react";
 import PokemonList from "./components/PokemonList/PokemonList";
+import PokemonDetails from "./components/PokemonDetails/PokemonDetails";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-// hello world
+const NotFoundPage = () => <div>404 - Not found</div>;
+
 export default function App() {
-  return <PokemonList />;
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={PokemonList} />
+        <Route exact path="/:pokemonName" component={PokemonDetails} />
+        <Route path="*" component={NotFoundPage} />
+      </Switch>
+    </Router>
+  );
 }
